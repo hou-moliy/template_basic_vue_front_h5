7d0599481c71d467c56cccf26b865c64299aab6a
侯元珍 <1337312569@qq.com>
2022-07-28 14:08:51 +0800

    [Feat]: 新增全局公共组件的工程化导入

---
+++ src/components/common/index.js
@@ -0,0 +1,8 @@
+// 此文件用于'main.js'一次性导入所有common下的组件作为公用组件，一般情况下不要修改！！！
+const componentsFiles = require.context('.', true, /\.js$/);
+const components = {};
+componentsFiles.keys().forEach(key => {
+  if (key === './index.js') { return; };
+  components[key.replace(/(\.\/|\.js|\/index |\/)/g, '')] = (componentsFiles(key).default || componentsFiles(key));
+});
+export default components;
--- src/main.js
+++ src/main.js
@@ -7,9 +7,15 @@ import 'lib-flexible/flexible';
 import 'normalize.css/normalize.css';
 import "./plugins";
 import "./permission";
-import Directives from './directives';
 import analysis from './utils/analysis';
+import Directives from './directives';
+import CommonComponents from "./components/common";
+// 全局指令注册，一般情况下不要修改！！！
 Vue.use(Directives);
+// 全局公用组件注册，一般情况下不要修改！！！
+Object.keys(CommonComponents).forEach((key) => {
+  Vue.prototype[`$${key}`] = CommonComponents[key].install;
+});
 //  cancelToken中的cancel函数
 Vue.$httpRequestList = [];
 Vue.prototype.$staticImgs = process.env.VUE_APP_STATIC_IMGS || '/resource';
